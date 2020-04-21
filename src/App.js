1f22e1386c3582fdd5f3ba12d906eef3e5f851ab
Hannah Karwat <hdkarwat@gmail.com>
2020-04-21 06:18:33 -0400

    commit 1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import {Home} from './Components/Home'
+import {TourneyInfo} from './Components/TourneyInfo'
+import {TeamInfo} from './Components/TeamInfo'
+function App() {
+  return (
+
+    <div className="container"> 
+    <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export class Home extends Component {
+
+    render(){
+        return(
+            <div className="mt-5 d-flex justify-content-left">
+                <h2>Welcome to Tourney Genie</h2>
+                <p> The tourney app you never knew you needed.</p>
+            </div>
+        )
+
+        
+    }
+}
