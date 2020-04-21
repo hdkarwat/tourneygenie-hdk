6e57814d2d70f5f7ed0ab88fb869dff7473dbf78
Hannah Karwat <hdkarwat@gmail.com>
2020-04-21 13:00:58 -0400

    table updates

--- src/Components/TeamInfo.jsx
+++ src/Components/TeamInfo.jsx
@@ -1,13 +1,48 @@
 import React, {Component} from 'react';
+import {Table} from 'react-bootstrap'; 
 
 export class TeamInfo extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state ={teams:[]}
+    }
+
+    componentDidMount() {
+        this.refreshList(); 
+    }
+    refreshList() {
+        this.setState({
+            teams:[{"TournamentID":1, "TeamName": "Muth", "TeamAgeGroup": "14U"}, 
+            {"TournamentID":2, "TeamName": "Mavs", "TeamAgeGroup": "16U"}
+        ]
+        })
+    }
     render(){
+
+    const {teams} = this.state; 
+
         return(
-            <div className="mt-5 d-flex justify-content-left">
-                <h2>This is the Team Info Page</h2>
-                
-            </div>
+           <Table className="mt-4" stripped bordered hover size="sm">
+               <thead>
+                    <tr>
+                        <th>TournamentID</th>
+                        <th>TeamName</th>
+                        <th>TeamAgeGroup</th>
+                    </tr>
+               </thead>
+
+               <tbody>
+                   {teams.map(team=> 
+                        <tr key = {team.TournamentID}> 
+                            <td>{team.TournamentID}</td>
+                            <td>{team.TeamName}</td>
+                            <td>{team.TeamAgeGroup}</td>
+                        </tr>
+                        )}
+               </tbody>
+
+           </Table>
         )
 
         
