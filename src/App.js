713e1c840cc8f6ac30deb99cf09036241c30e3a8
Hannah Karwat <hdkarwat@gmail.com>
2020-04-21 06:49:45 -0400

    commit 2

--- src/App.js
+++ src/App.js
@@ -5,12 +5,30 @@ import './App.css';
 import {Home} from './Components/Home'
 import {TourneyInfo} from './Components/TourneyInfo'
 import {TeamInfo} from './Components/TeamInfo'
+
+import Button from 'react-bootstrap/Button'
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 function App() {
   return (
 
+    <BrowserRouter>
     <div className="container"> 
-    <Home/>
+    <Button variant='primary'> Bootstrap Button</Button>
+
+    <h3 className="m-3 d-flex justify-content-center"> Tourney Genie</h3>
+
+    <h5 className="m-3 d-flex justify-content-center"> Pocket Tournament Director </h5>
+
+
+
+    <Switch>
+      <Route path='/' component={Home} exact/>
+      <Route path='/TourneyInfo' component={TourneyInfo} exact/>
+      <Route path='/TeamInfo' component={TeamInfo} exact/>
+    </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
